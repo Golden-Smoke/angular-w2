eb24856f54f6eedfd3795df205437ad5598e8572
Golden-Smoke <plamengeorgiev967@gmail.com>
2024-04-23 21:26:12 +0300

    Initializing books array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { bookList} from '../booklist';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'homework2-2201681046';
+  books = bookList;
 }
---
+++ src/booklist.ts
@@ -0,0 +1,18 @@
+type Book = {
+    title: string,
+    description: string,
+    authors: string
+}
+
+export const bookList: Book[] = [
+    {
+        title:"Harry Potter",
+        description:"Some short desc for Harry Potter",
+        authors:"Tolkein"
+    },
+    {
+        title:"Lord of the Rings",
+        description:"Some short desc for Lord of The Rings",
+        authors:"Tolkeins"
+    }
+]
